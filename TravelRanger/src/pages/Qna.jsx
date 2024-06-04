63066c6cd9aa737d00632414e2716e91dc8cb30f
Anupam <ianupam1509@gmail.com>
2024-06-04 23:23:48 +0530

    qna modified

--- TravelRanger/src/pages/Qna.jsx
+++ TravelRanger/src/pages/Qna.jsx
@@ -8,16 +8,23 @@ const Qna = () => {
       <div className={style_qna.container}>
         <div className={style_qna.top}>
           <div className={style_qna.left}>
-            <h3> Why MakeMyTrip?</h3>
-            <p>Established in 2000, MakeMyTrip has since positioned itself as one of <br />  the leading companies,<br />  providing great offers, competitive airfares, exclusive discounts, and a seamless online booking experience to many <br /> of its customers. The experience of booking your flight tickets, hotel <br /> stay, and holiday package through our desktop site or mobile app can be<br />  done with complete ease and no hassles at all. We also deliver amazing <br /> offers, such as Instant Discounts, Fare Calendar, MyRewardsProgram, <br /> MyWallet, and many more while updating them from time to time <br /> to better suit our customers’ evolving needs and demands.</p>
-            
+            <h3>Q. Why TravelRanger?</h3>
+            <p>Established in 2024, Travel Ranger has since positioned itself as one of <br />
+              the leading neighbourhood friendly booking website, providing great offers,<br />
+              competitive airfares, exclusive discounts, and a seamless online booking <br />
+              experience to many of its customers. The experience of booking your flight <br />
+              tickets, hotel, homestay, and holiday package through our desktop site can be<br />
+              done with complete ease and no hassles at all. We also deliver amazing <br />
+              offers, such as Instant Discounts, Fare Calendar, MyRewardsProgram, <br />
+              MyWallet, and many more while updating them from time to time to better <br />
+              suit our customers’ evolving needs and demands.</p>
           </div>
 
           <div className={style_qna.right}>
-            <h4>Q. How to find the cheapest hotel deals in any city?</h4>
+            <h3>Q. How to find the cheapest hotel deals in any city?</h3>
 
             <p>
-              A. MakeMyTrip, being the best hotel-booking site in the country,
+              A. Travel Ranger, being the best neighbourhood hotel-booking site in the country,
               offers several discounts on budget <br /> hotels as well. If you
               are looking for the cheapest hotels with amazing deals on the app,
               you can tap on <br /> Sort & Filter option and drag down the Price
